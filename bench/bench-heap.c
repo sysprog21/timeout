de56382cbda4c648ea87e74b4d89b12e1687d774
Jim Huang <jserv@biilabs.io>
2021-04-10 23:50:35 +0800

    Apply editorial changes
    Use consistent style and add explicit license terms (MIT).

--- bench/bench-heap.c
+++ bench/bench-heap.c
@@ -27,61 +27,86 @@
 #ifndef _MIN_HEAP_H_
 #define _MIN_HEAP_H_
 
-#include <stdlib.h>
 #include <err.h>
+#include <stdlib.h>
+
 #include "timeout.h"
+
 #include "bench.h"
 
 #define min_heap_idx interval
 
 typedef timeout_t min_heap_idx_t;
 
-typedef struct min_heap
-{
-    struct timeout** p;
+typedef struct min_heap {
+    struct timeout **p;
     unsigned n, a;
     timeout_t curtime;
 } min_heap_t;
 
-static inline void           min_heap_ctor(min_heap_t* s);
-static inline void           min_heap_dtor(min_heap_t* s);
-static inline void           min_heap_elem_init(struct timeout* e);
-static inline int            min_heap_elem_greater(struct timeout *a, struct timeout *b);
-static inline int            min_heap_empty(min_heap_t* s);
-static inline unsigned       min_heap_size(min_heap_t* s);
-static inline struct timeout*  min_heap_top(min_heap_t* s);
-static inline int            min_heap_reserve(min_heap_t* s, unsigned n);
-static inline int            min_heap_push(min_heap_t* s, struct timeout* e);
-static inline struct timeout*  min_heap_pop(min_heap_t* s);
-static inline int            min_heap_erase(min_heap_t* s, struct timeout* e);
-static inline void           min_heap_shift_up_(min_heap_t* s, unsigned hole_index, struct timeout* e);
-static inline void           min_heap_shift_down_(min_heap_t* s, unsigned hole_index, struct timeout* e);
+static inline void min_heap_ctor(min_heap_t *s);
+static inline void min_heap_dtor(min_heap_t *s);
+static inline void min_heap_elem_init(struct timeout *e);
+static inline int min_heap_elem_greater(struct timeout *a, struct timeout *b);
+static inline int min_heap_empty(min_heap_t *s);
+static inline unsigned min_heap_size(min_heap_t *s);
+static inline struct timeout *min_heap_top(min_heap_t *s);
+static inline int min_heap_reserve(min_heap_t *s, unsigned n);
+static inline int min_heap_push(min_heap_t *s, struct timeout *e);
+static inline struct timeout *min_heap_pop(min_heap_t *s);
+static inline int min_heap_erase(min_heap_t *s, struct timeout *e);
+static inline void min_heap_shift_up_(min_heap_t *s,
+                                      unsigned hole_index,
+                                      struct timeout *e);
+static inline void min_heap_shift_down_(min_heap_t *s,
+                                        unsigned hole_index,
+                                        struct timeout *e);
 
 int min_heap_elem_greater(struct timeout *a, struct timeout *b)
 {
     return a->expires > b->expires;
 }
 
-void min_heap_ctor(min_heap_t* s) { s->p = 0; s->n = 0; s->a = 0; }
-void min_heap_dtor(min_heap_t* s) { if(s->p) free(s->p); }
-void min_heap_elem_init(struct timeout* e) { e->min_heap_idx = -1; }
-int min_heap_empty(min_heap_t* s) { return 0u == s->n; }
-unsigned min_heap_size(min_heap_t* s) { return s->n; }
-struct timeout* min_heap_top(min_heap_t* s) { return s->n ? *s->p : 0; }
-
-int min_heap_push(min_heap_t* s, struct timeout* e)
+void min_heap_ctor(min_heap_t *s)
 {
-    if(min_heap_reserve(s, s->n + 1))
+    s->p = 0;
+    s->n = 0;
+    s->a = 0;
+}
+void min_heap_dtor(min_heap_t *s)
+{
+    if (s->p)
+        free(s->p);
+}
+void min_heap_elem_init(struct timeout *e)
+{
+    e->min_heap_idx = -1;
+}
+int min_heap_empty(min_heap_t *s)
+{
+    return 0u == s->n;
+}
+unsigned min_heap_size(min_heap_t *s)
+{
+    return s->n;
+}
+struct timeout *min_heap_top(min_heap_t *s)
+{
+    return s->n ? *s->p : 0;
+}
+
+int min_heap_push(min_heap_t *s, struct timeout *e)
+{
+    if (min_heap_reserve(s, s->n + 1))
         return -1;
     min_heap_shift_up_(s, s->n++, e);
     return 0;
 }
 
-struct timeout* min_heap_pop(min_heap_t* s)
+struct timeout *min_heap_pop(min_heap_t *s)
 {
-    if(s->n)
-    {
-        struct timeout* e = *s->p;
+    if (s->n) {
+        struct timeout *e = *s->p;
         min_heap_shift_down_(s, 0u, s->p[--s->n]);
         e->min_heap_idx = -1;
         return e;
@@ -89,36 +114,34 @@ struct timeout* min_heap_pop(min_heap_t* s)
     return 0;
 }
 
-int min_heap_erase(min_heap_t* s, struct timeout* e)
+int min_heap_erase(min_heap_t *s, struct timeout *e)
 {
-    if(((min_heap_idx_t)-1) != e->min_heap_idx)
-    {
+    if (((min_heap_idx_t) -1) != e->min_heap_idx) {
         struct timeout *last = s->p[--s->n];
         unsigned parent = (e->min_heap_idx - 1) / 2;
-	/* we replace e with the last element in the heap.  We might need to
-	   shift it upward if it is less than its parent, or downward if it is
-	   greater than one or both its children. Since the children are known
-	   to be less than the parent, it can't need to shift both up and
-	   down. */
+        /* we replace e with the last element in the heap.  We might need to
+           shift it upward if it is less than its parent, or downward if it is
+           greater than one or both its children. Since the children are known
+           to be less than the parent, it can't need to shift both up and
+           down. */
         if (e->min_heap_idx > 0 && min_heap_elem_greater(s->p[parent], last))
-             min_heap_shift_up_(s, e->min_heap_idx, last);
+            min_heap_shift_up_(s, e->min_heap_idx, last);
         else
-             min_heap_shift_down_(s, e->min_heap_idx, last);
+            min_heap_shift_down_(s, e->min_heap_idx, last);
         e->min_heap_idx = -1;
         return 0;
     }
     return -1;
 }
 
-int min_heap_reserve(min_heap_t* s, unsigned n)
+int min_heap_reserve(min_heap_t *s, unsigned n)
 {
-    if(s->a < n)
-    {
-        struct timeout** p;
+    if (s->a < n) {
+        struct timeout **p;
         unsigned a = s->a ? s->a * 2 : 8;
-        if(a < n)
+        if (a < n)
             a = n;
-        if(!(p = (struct timeout**)realloc(s->p, a * sizeof *p)))
+        if (!(p = (struct timeout **) realloc(s->p, a * sizeof *p)))
             return -1;
         s->p = p;
         s->a = a;
@@ -126,11 +149,10 @@ int min_heap_reserve(min_heap_t* s, unsigned n)
     return 0;
 }
 
-void min_heap_shift_up_(min_heap_t* s, unsigned hole_index, struct timeout* e)
+void min_heap_shift_up_(min_heap_t *s, unsigned hole_index, struct timeout *e)
 {
     unsigned parent = (hole_index - 1) / 2;
-    while(hole_index && min_heap_elem_greater(s->p[parent], e))
-    {
+    while (hole_index && min_heap_elem_greater(s->p[parent], e)) {
         (s->p[hole_index] = s->p[parent])->min_heap_idx = hole_index;
         hole_index = parent;
         parent = (hole_index - 1) / 2;
@@ -138,25 +160,27 @@ void min_heap_shift_up_(min_heap_t* s, unsigned hole_index, struct timeout* e)
     (s->p[hole_index] = e)->min_heap_idx = hole_index;
 }
 
-void min_heap_shift_down_(min_heap_t* s, unsigned hole_index, struct timeout* e)
+void min_heap_shift_down_(min_heap_t *s, unsigned hole_index, struct timeout *e)
 {
     unsigned min_child = 2 * (hole_index + 1);
-    while(min_child <= s->n)
-	{
-        min_child -= min_child == s->n || min_heap_elem_greater(s->p[min_child], s->p[min_child - 1]);
-        if(!(min_heap_elem_greater(e, s->p[min_child])))
+    while (min_child <= s->n) {
+        min_child -=
+            min_child == s->n ||
+            min_heap_elem_greater(s->p[min_child], s->p[min_child - 1]);
+        if (!(min_heap_elem_greater(e, s->p[min_child])))
             break;
         (s->p[hole_index] = s->p[min_child])->min_heap_idx = hole_index;
         hole_index = min_child;
         min_child = 2 * (hole_index + 1);
-	}
-    min_heap_shift_up_(s, hole_index,  e);
+    }
+    min_heap_shift_up_(s, hole_index, e);
 }
 
 #endif /* _MIN_HEAP_H_ */
 
 
-static void *init(struct timeout *timeout, size_t count, int verbose) {
+static void *init(struct timeout *timeout, size_t count, int verbose)
+{
     min_heap_t *H;
     size_t i;
 
@@ -174,7 +198,8 @@ static void *init(struct timeout *timeout, size_t count, int verbose) {
 } /* init() */
 
 
-static void add(void *ctx, struct timeout *to, timeout_t expires) {
+static void add(void *ctx, struct timeout *to, timeout_t expires)
+{
     min_heap_t *H = ctx;
     min_heap_erase(H, to);
     to->expires = H->curtime + expires;
@@ -183,12 +208,14 @@ static void add(void *ctx, struct timeout *to, timeout_t expires) {
 } /* add() */
 
 
-static void del(void *ctx, struct timeout *to) {
+static void del(void *ctx, struct timeout *to)
+{
     min_heap_erase(ctx, to);
 } /* del() */
 
 
-static struct timeout *get(void *ctx) {
+static struct timeout *get(void *ctx)
+{
     min_heap_t *H = ctx;
     struct timeout *to;
 
@@ -199,38 +226,41 @@ static struct timeout *get(void *ctx) {
 } /* get() */
 
 
-static void update(void *ctx, timeout_t ts) {
+static void update(void *ctx, timeout_t ts)
+{
     min_heap_t *H = ctx;
     H->curtime = ts;
 } /* update() */
 
 
-static void check(void *ctx) {
+static void check(void *ctx)
+{
     return;
 } /* check() */
 
 
-static int empty(void *ctx) {
+static int empty(void *ctx)
+{
     min_heap_t *H = ctx;
 
     return (NULL == min_heap_top(H));
 } /* empty() */
 
 
-static void destroy(void *H) {
+static void destroy(void *H)
+{
     free(H);
     return;
 } /* destroy() */
 
 
 const struct benchops benchops = {
-    .init    = &init,
-    .add     = &add,
-    .del     = &del,
-    .get     = &get,
-    .update  = &update,
-    .check   = &check,
-    .empty   = &empty,
+    .init = &init,
+    .add = &add,
+    .del = &del,
+    .get = &get,
+    .update = &update,
+    .check = &check,
+    .empty = &empty,
     .destroy = &destroy,
 };
-
--- timeout.h
+++ timeout.h
@@ -1,6 +1,5 @@
-/* ==========================================================================
+/*
  * timeout.h - Tickless hierarchical timing wheel.
- * --------------------------------------------------------------------------
  * Copyright (c) 2013, 2014  William Ahern
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -21,30 +20,26 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
  * USE OR OTHER DEALINGS IN THE SOFTWARE.
- * ==========================================================================
  */
+
 #ifndef TIMEOUT_H
 #define TIMEOUT_H
 
-#include <stdbool.h>    /* bool */
-#include <stdio.h>      /* FILE */
+#include <stdbool.h> /* bool */
+#include <stdio.h>   /* FILE */
+#include <inttypes.h> /* PRIu64 PRIx64 PRIX64 uint64_t */
 
-#include <inttypes.h>   /* PRIu64 PRIx64 PRIX64 uint64_t */
-
-#include <sys/queue.h>  /* TAILQ(3) */
+#include <sys/queue.h> /* TAILQ(3) */
 
 
-/*
- * V E R S I O N  I N T E R F A C E S
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* version interfaces */
 
 #if !defined TIMEOUT_PUBLIC
 #define TIMEOUT_PUBLIC
 #endif
 
 #define TIMEOUT_VERSION TIMEOUT_V_REL
-#define TIMEOUT_VENDOR  "william@25thandClement.com"
+#define TIMEOUT_VENDOR "william@25thandClement.com"
 
 #define TIMEOUT_V_REL 0x20160226
 #define TIMEOUT_V_ABI 0x20160224
@@ -61,10 +56,7 @@ TIMEOUT_PUBLIC int timeout_v_abi(void);
 TIMEOUT_PUBLIC int timeout_v_api(void);
 
 
-/*
- * I N T E G E R  T Y P E  I N T E R F A C E S
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* integer type interfaces */
 
 #define TIMEOUT_C(n) UINT64_C(n)
 #define TIMEOUT_PRIu PRIu64
@@ -80,63 +72,62 @@ typedef uint64_t timeout_t;
 #define timeout_error_t int /* for documentation purposes */
 
 
-/*
- * C A L L B A C K  I N T E R F A C E
+/* callback interface
  *
  * Callback function parameters unspecified to make embedding into existing
  * applications easier.
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ */
 
 #ifndef TIMEOUT_CB_OVERRIDE
 struct timeout_cb {
-	void (*fn)();
-	void *arg;
+    void (*fn)();
+    void *arg;
 }; /* struct timeout_cb */
 #endif
 
-/*
- * T I M E O U T  I N T E R F A C E S
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* timeout interfaces */
 
 #ifndef TIMEOUT_DISABLE_INTERVALS
 #define TIMEOUT_INT 0x01 /* interval (repeating) timeout */
 #endif
 #define TIMEOUT_ABS 0x02 /* treat timeout values as absolute */
 
-#define TIMEOUT_INITIALIZER(flags) { (flags) }
+#define TIMEOUT_INITIALIZER(flags) \
+    {                              \
+        (flags)                    \
+    }
 
-#define timeout_setcb(to, fn, arg) do { \
-	(to)->callback.fn = (fn);       \
-	(to)->callback.arg = (arg);     \
-} while (0)
+#define timeout_setcb(to, fn, arg)  \
+    do {                            \
+        (to)->callback.fn = (fn);   \
+        (to)->callback.arg = (arg); \
+    } while (0)
 
 struct timeout {
-	int flags;
+    int flags;
 
-	timeout_t expires;
-	/* absolute expiration time */
+    timeout_t expires;
+    /* absolute expiration time */
 
-	struct timeout_list *pending;
-	/* timeout list if pending on wheel or expiry queue */
+    struct timeout_list *pending;
+    /* timeout list if pending on wheel or expiry queue */
 
-	TAILQ_ENTRY(timeout) tqe;
-	/* entry member for struct timeout_list lists */
+    TAILQ_ENTRY(timeout) tqe;
+    /* entry member for struct timeout_list lists */
 
 #ifndef TIMEOUT_DISABLE_CALLBACKS
-	struct timeout_cb callback;
-	/* optional callback information */
+    struct timeout_cb callback;
+    /* optional callback information */
 #endif
 
 #ifndef TIMEOUT_DISABLE_INTERVALS
-	timeout_t interval;
-	/* timeout interval if periodic */
+    timeout_t interval;
+    /* timeout interval if periodic */
 #endif
 
 #ifndef TIMEOUT_DISABLE_RELATIVE_ACCESS
-	struct timeouts *timeouts;
-	/* timeouts collection if member of */
+    struct timeouts *timeouts;
+    /* timeouts collection if member of */
 #endif
 }; /* struct timeout */
 
@@ -147,7 +138,7 @@ TIMEOUT_PUBLIC struct timeout *timeout_init(struct timeout *, int);
 #ifndef TIMEOUT_DISABLE_RELATIVE_ACCESS
 TIMEOUT_PUBLIC bool timeout_pending(struct timeout *);
 /* true if on timing wheel, false otherwise */
- 
+
 TIMEOUT_PUBLIC bool timeout_expired(struct timeout *);
 /* true if on expired queue, false otherwise */
 
@@ -155,10 +146,7 @@ TIMEOUT_PUBLIC void timeout_del(struct timeout *);
 /* remove timeout from any timing wheel (okay if not member of any) */
 #endif
 
-/*
- * T I M I N G  W H E E L  I N T E R F A C E S
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* timing wheel interfaces */
 
 struct timeouts;
 
@@ -180,7 +168,9 @@ TIMEOUT_PUBLIC void timeouts_step(struct timeouts *, timeout_t);
 TIMEOUT_PUBLIC timeout_t timeouts_timeout(struct timeouts *);
 /* return interval to next required update */
 
-TIMEOUT_PUBLIC void timeouts_add(struct timeouts *, struct timeout *, timeout_t);
+TIMEOUT_PUBLIC void timeouts_add(struct timeouts *,
+                                 struct timeout *,
+                                 timeout_t);
 /* add timeout to timing wheel */
 
 TIMEOUT_PUBLIC void timeouts_del(struct timeouts *, struct timeout *);
@@ -196,40 +186,45 @@ TIMEOUT_PUBLIC bool timeouts_expired(struct timeouts *);
 /* return true if any timeouts on expired queue */
 
 TIMEOUT_PUBLIC bool timeouts_check(struct timeouts *, FILE *);
-/* return true if invariants hold. describes failures to optional file handle. */
+/* return true if invariants hold. describes failures to optional file handle.
+ */
 
 #define TIMEOUTS_PENDING 0x10
 #define TIMEOUTS_EXPIRED 0x20
-#define TIMEOUTS_ALL     (TIMEOUTS_PENDING|TIMEOUTS_EXPIRED)
-#define TIMEOUTS_CLEAR   0x40
+#define TIMEOUTS_ALL (TIMEOUTS_PENDING | TIMEOUTS_EXPIRED)
+#define TIMEOUTS_CLEAR 0x40
 
-#define TIMEOUTS_IT_INITIALIZER(flags) { (flags), 0, 0, 0, 0 }
+#define TIMEOUTS_IT_INITIALIZER(flags) \
+    {                                  \
+        (flags), 0, 0, 0, 0            \
+    }
 
-#define TIMEOUTS_IT_INIT(cur, _flags) do {                              \
-	(cur)->flags = (_flags);                                        \
-	(cur)->pc = 0;                                                  \
-} while (0)
+#define TIMEOUTS_IT_INIT(cur, _flags) \
+    do {                              \
+        (cur)->flags = (_flags);      \
+        (cur)->pc = 0;                \
+    } while (0)
 
 struct timeouts_it {
-	int flags;
-	unsigned pc, i, j;
-	struct timeout *to;
+    int flags;
+    unsigned pc, i, j;
+    struct timeout *to;
 }; /* struct timeouts_it */
 
-TIMEOUT_PUBLIC struct timeout *timeouts_next(struct timeouts *, struct timeouts_it *);
+TIMEOUT_PUBLIC struct timeout *timeouts_next(struct timeouts *,
+                                             struct timeouts_it *);
 /* return next timeout in pending wheel or expired queue. caller can delete
  * the returned timeout, but should not otherwise manipulate the timing
  * wheel. in particular, caller SHOULD NOT delete any other timeout as that
  * could invalidate cursor state and trigger a use-after-free.
  */
 
-#define TIMEOUTS_FOREACH(var, T, flags)                                 \
-	struct timeouts_it _it = TIMEOUTS_IT_INITIALIZER((flags));      \
-	while (((var) = timeouts_next((T), &_it)))
+#define TIMEOUTS_FOREACH(var, T, flags)                        \
+    struct timeouts_it _it = TIMEOUTS_IT_INITIALIZER((flags)); \
+    while (((var) = timeouts_next((T), &_it)))
 
 
-/*
- * B O N U S  W H E E L  I N T E R F A C E S
+/* bonus wheel interfaces
  *
  * I usually use floating point timeouts in all my code, but it's cleaner to
  * separate it to keep the core algorithmic code simple.
@@ -242,12 +237,12 @@ TIMEOUT_PUBLIC struct timeout *timeouts_next(struct timeouts *, struct timeouts_
 #include <math.h> /* ceil(3) */
 
 #define timeouts_f2i(T, f) \
-	((timeout_t)ceil((f) * timeouts_hz((T)))) /* prefer late expiration over early */
+    ((timeout_t) ceil(     \
+        (f) *timeouts_hz((T)))) /* prefer late expiration over early */
 
-#define timeouts_i2f(T, i) \
-	((double)(i) / timeouts_hz((T)))
+#define timeouts_i2f(T, i) ((double) (i) / timeouts_hz((T)))
 
 #define timeouts_addf(T, to, timeout) \
-	timeouts_add((T), (to), timeouts_f2i((T), (timeout)))
+    timeouts_add((T), (to), timeouts_f2i((T), (timeout)))
 
 #endif /* TIMEOUT_H */
